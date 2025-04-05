f2a4a438d1a3482f9c1a26062b3368a3fb5fb1f1
Dasha <75611738+DashaKam@users.noreply.github.com>
2025-04-05 20:07:27 +0700

    Update App.js

--- frontend/App.js
+++ frontend/App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import RegistrationScreen from './RegistrationScreen'; 
 import LoginScreen from './LoginScreen';
+import SuccessScreen from './SuccessScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +13,7 @@ const App = () => {
       <Stack.Navigator initialRouteName="Registration">
         <Stack.Screen name="Registration" component={RegistrationScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Success" component={SuccessScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
